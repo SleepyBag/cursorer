f6471c421e23fe63238eb3de8852cb6852862540
Qianming Xue <xueqianming200@gmail.com>
2023-08-19 22:34:16 +0800

    Refactor: cursor scheme

---
+++ src/cursorScheme.js
@@ -0,0 +1,101 @@
+const cursorKeyNames = ["Arrow", "Help", "AppStarting", "Wait", "Crosshair", "IBeam", "NWPen", "No", "SizeNS", "SizeWE", "SizeNWSE", "SizeNESW", "SizeAll", "UpArrow", "Hand", "Person", "Pin"];
+const cursorSelectionPath = 'HKCU\\Control Panel\\Cursors';
+const cursorSchemesPath = 'HKCU\\Control Panel\\Cursors\\Schemes';
+
+class CursorScheme {
+    constructor(name, paths) {
+        this.name = name;
+
+        paths = paths.split(',');
+        this.paths = {};
+        for (let i = 0; i < paths.length; i++) {
+            const path = paths[i];
+            const keyName = cursorKeyNames[i];
+            this.paths[keyName] = path;
+        }
+
+        this.normalCursorPath = this.paths.Arrow;
+        this.handCursorPath = this.paths.Hand;
+        this.appStartingCursorPath = this.paths.AppStarting;
+        this.waitCursorPath = this.paths.Wait;
+    }
+
+    toRegValue() {
+        const valuesToPut = {};
+        for (const keyName in this.paths) {
+            const path = this.paths[keyName];
+            if (path.length > 0) {
+                valuesToPut[keyName] = { value: path, type: 'REG_EXPAND_SZ' };
+            }
+        }
+        return valuesToPut;
+    }
+}
+
+export class CursorSchemeList {
+    constructor() {
+        this.cursorSchemes = {};
+    }
+
+    isInstalled(cursorSchemeName) {
+        return this.cursorSchemes[cursorSchemeName] !== undefined;
+    }
+
+    get(cursorSchemeName) {
+        const cursorScheme = this.cursorSchemes[cursorSchemeName];
+        if (cursorScheme === undefined) {
+            throw new Error(`${cursorSchemeName} not found!`);
+        }
+        return cursorScheme;
+    }
+
+    async apply(cursorSchemeName) {
+        const cursorScheme = this.get(cursorSchemeName);
+        console.log(`Setting cursor scheme to "${cursorScheme.name}"`);
+        const valuesToPut = cursorScheme.toRegValue();
+        regedit.putValue({ [cursorSelectionPath]: valuesToPut }, error => {
+            if (error !== undefined) {
+                console.log(`Error when setting cursor scheme: ${error}`);
+            } else {
+                console.log(`Successfully set cursor scheme to ${cursorScheme.name}`);
+                exec(`.\\utils\\RefreshCursor.exe`, { encoding: "utf8" }, (error, stdout, stderr) => {
+                    console.log("RefreshCursor finished");
+                    console.log(`Error: ${error}`);
+                    console.log(`Stdout: ${stdout}`);
+                    console.log(`Stderr: ${stderr}`);
+                });
+            }
+        });
+    }
+
+    async delete(cursorSchemeName) {
+        const cursorScheme = this.get(cursorSchemeName);
+        if (confirm(`Deleting cursor scheme ${cursorScheme.name}?`)) {
+            console.log(`Deleting cursor scheme to "${cursorScheme.name}"`);
+            regedit.deleteValue(cursorSchemesPath + '\\' + cursorScheme.name, error => {
+                if (error !== undefined) {
+                    console.log(`Error when deleting cursor scheme: ${error}`);
+                } else {
+                    console.log(`Successfully deleted cursor scheme ${cursorScheme.name}.`);
+                }
+            });
+        }
+        await this.refresh();
+    }
+
+    async refresh() {
+        const schemesInReg = (await regedit.list(cursorSchemesPath))[cursorSchemesPath].values;
+        for (const schemeName in this.cursorSchemes) {
+            if (schemesInReg[schemeName] === undefined) {
+                delete this.cursorSchemes[schemeName];
+            }
+        }
+        for (const schemeName in schemesInReg) {
+            this.cursorSchemes[schemeName] = new CursorScheme(schemeName, schemesInReg[schemeName].value);
+        }
+    }
+
+    [Symbol.iterator]() {
+        return Object.values(this.cursorSchemes).values();
+    }
+}
--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,10 @@
 import { AniCacher } from './aniCache.js'
 import { DownloadList } from './download.js'
+import { CursorSchemeList } from './cursorScheme.js'
 
-const cursorSchemes = Vue.reactive({});
+const cursorSchemes = Vue.reactive(new CursorSchemeList());
 const aniCacher = Vue.reactive(new AniCacher(rootPath));
 
-const cursorKeyNames = ["Arrow", "Help", "AppStarting", "Wait", "Crosshair", "IBeam", "NWPen", "No", "SizeNS", "SizeWE", "SizeNWSE", "SizeNESW", "SizeAll", "UpArrow", "Hand", "Person", "Pin"];
-const cursorSchemesPath = 'HKCU\\Control Panel\\Cursors\\Schemes';
 const cursorSelectionPath = 'HKCU\\Control Panel\\Cursors';
 
 const downloadItems = Vue.reactive(new DownloadList());
@@ -41,96 +40,14 @@ const links = [
   new Link("Pling", "https://www.pling.com/browse?cat=107"),
 ];
 
-class CursorScheme {
-  constructor(name) {
-    this.name = name;
-  }
-
-  toRegValue() {
-    const valuesToPut = {};
-    for (const keyName in this.paths) {
-      const path = this.paths[keyName];
-      if (path.length > 0) {
-        valuesToPut[keyName] = { value: path, type: 'REG_EXPAND_SZ' };
-      }
-    }
-    return valuesToPut;
-  }
-
-  static async from(name, paths) {
-    const scheme = new CursorScheme(name);
-    scheme.name = name;
-    paths = paths.split(',');
-    scheme.paths = {};
-    for (let i = 0; i < paths.length; i++) {
-      const path = paths[i];
-      const keyName = cursorKeyNames[i];
-      scheme.paths[keyName] = path;
-    }
-
-    scheme.normalCursorPath = scheme.paths.Arrow;
-    scheme.handCursorPath = scheme.paths.Hand;
-    scheme.appStartingCursorPath = scheme.paths.AppStarting;
-    scheme.waitCursorPath = scheme.paths.Wait;
-    return scheme
-  }
-}
-
-// link to websites
-const buttons = document.getElementsByTagName("button");
-for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-    button.onclick = () => openWebsiteInNewWindow(button.getAttribute("target"));
-}
-
 function openWebsiteInNewWindow(url) {
   // Create the browser window.
   const newWindow = window.open(url, '', 'height=768,width=1024');
 }
 
-async function applyCursorScheme(cursorScheme) {
-  console.log(`Setting cursor scheme to "${cursorScheme.name}"`);
-  const valuesToPut = cursorScheme.toRegValue();
-  regedit.putValue({ [cursorSelectionPath] : valuesToPut }, error => {
-    if (error !== undefined) {
-      console.log(`Error when setting cursor scheme: ${error}`);
-    } else {
-      console.log(`Successfully set cursor scheme to ${cursorScheme.name}`);
-      exec(`.\\utils\\RefreshCursor.exe`, {encoding: "utf8"}, (error, stdout, stderr) => {
-        console.log("RefreshCursor finished");
-        console.log(`Error: ${error}`);
-        console.log(`Stdout: ${stdout}`);
-        console.log(`Stderr: ${stderr}`);
-      });
-    }
-  });
-}
-
-async function deleteCursorScheme(cursorScheme) {
-  if (confirm(`Deleting cursor scheme ${cursorScheme.name}?`)) {
-    console.log(`Deleting cursor scheme to "${cursorScheme.name}"`);
-    regedit.deleteValue(cursorSchemesPath + '\\' + cursorScheme.name, error => {
-      if (error !== undefined) {
-        console.log(`Error when deleting cursor scheme: ${error}`);
-      } else {
-        console.log(`Successfully deleted cursor scheme ${cursorScheme.name}.`);
-      }
-    });
-  }
-  await refresh();
-}
-
-async function refreshCursorSchemes() {
-  const schemesInReg = (await regedit.list(cursorSchemesPath))[cursorSchemesPath].values;
-  for (const schemeName in schemesInReg) {
-    cursorSchemes[schemeName] = await CursorScheme.from(schemeName, schemesInReg[schemeName].value);
-  }
-}
-
 async function refresh() {
-  await refreshCursorSchemes(cursorSchemes);
-  for (const cursorSchemeName in cursorSchemes) {
-    const cursorScheme = cursorSchemes[cursorSchemeName];
+  await cursorSchemes.refresh();
+  for (const cursorScheme of cursorSchemes) {
     await aniCacher.initializeCursorPath(cursorScheme.normalCursorPath);
     await aniCacher.initializeCursorPath(cursorScheme.handCursorPath);
     await aniCacher.initializeCursorPath(cursorScheme.appStartingCursorPath);
@@ -161,10 +78,6 @@ async function setCursorSize(newCursorSize) {
   });
 }
 
-function isInstalled(installationItem) {
-  return Object.values(cursorSchemes).some(scheme => scheme.name === installationItem.name);
-}
-
 var cursorSize = await getCursorSize();
 
 const vueApp = Vue.createApp({
@@ -174,9 +87,6 @@ const vueApp = Vue.createApp({
       cursorSchemes: cursorSchemes,
       downloadItems: downloadItems,
       aniCacher: aniCacher,
-      isInstalled: isInstalled,
-      applyCursorScheme: applyCursorScheme,
-      deleteCursorScheme: deleteCursorScheme,
       openWebsiteInNewWindow: openWebsiteInNewWindow,
       cursorSize: cursorSize,
       setCursorSize: setCursorSize,
